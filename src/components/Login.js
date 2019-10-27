0744003874ead9d7e50c3bd00d8a8dd382e44a6a
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-10-27 14:08:55 +0200

    Created custom dropdown

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,66 +2,87 @@ import React, { Component } from "react"
 import logo from "../logo.png"
 import { connect } from "react-redux"
 import { handleGetUsers } from "../actions/users"
-import styled from "styled-components"
 import { authenticateUser } from "../actions/authedUser"
+import { Dropdown } from "react-bootstrap"
+import { withRouter } from "react-router-dom"
 
 class Login extends Component {
-
   state = {
     user: null
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.dispatch(handleGetUsers())
   }
 
-  handleChangeUser = (e) =>{
-    this.setState({
-      user: e.target.value
-    })
+  handleChangeUser = user => {
+    this.setState({ user })
   }
 
   isDisabled = () => !Object.keys(this.props.users).includes(this.state.user)
 
-  handleSubmit = (e) => {
-    e.preventDefault()
+  handleSubmit = () => {
     this.props.dispatch(authenticateUser(this.state.user))
+    this.props.history.push("/")
   }
 
   render() {
+    const { users } = this.props
+
     return (
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-10 col-md-7 col-lg-5">
             <div className="card mt-5">
               <div className="card-header text-center">
-                <h5 className="header-welcome">Welcome to the Would You Rather App!</h5>
+                <h5 className="header-welcome">
+                  Welcome to the Would You Rather App!
+                </h5>
                 <p className="lead header-lead">Please sign in to continue</p>
               </div>
               <div className="card-body text-center">
                 <img src={logo} alt="React logo" />
                 <p className="card-text font-weight-bold text-info">Sign in</p>
-                <form onSubmit={this.handleSubmit}>
-                  <select className="form-control"
-                    defaultValue="Select User"
-                    onChange={this.handleChangeUser}>
-                    <option value="Select User" disabled>
-                      Select User...
-                    </option>
-                    {Object.keys(this.props.users).map(user => (
-                      <UserOption key={user} value={user}
-                        user={this.props.users[user]}>
-                        {this.props.users[user].name}
-                      </UserOption>
-                    ))}
-                  </select>
-                  <input
-                    type="submit"
+                <div>
+                  <Dropdown>
+                    <Dropdown.Toggle className="d-block" id="dropdown-basic">
+                      {this.state.user ? (
+                        <>
+                          <img
+                            src={this.props.users[this.state.user].avatarURL}
+                            alt={this.state.user}
+                            className="user-option-avatar"
+                          />
+                          <span className="ml-1">
+                            {this.props.users[this.state.user].name}
+                          </span>
+                        </>
+                      ) : (
+                        "Select User..."
+                      )}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      {Object.keys(users).map(user => (
+                        <Dropdown.Item
+                          key={user}
+                          onClick={() => this.handleChangeUser(user)}>
+                          <img
+                            src={users[user].avatarURL}
+                            alt={user}
+                            className="user-option-avatar"/>
+                          <span className="ml-1">{users[user].name}</span>
+                        </Dropdown.Item>
+                      ))}
+                    </Dropdown.Menu>
+                  </Dropdown>
+                  <button
+                    onClick={this.handleSubmit}
                     className="btn btn-info btn-block mt-2"
-                    value="Sign in"
-                    disabled={this.isDisabled()}
-                  />
-                </form>
+                    value="Sign In"
+                    disabled={this.isDisabled()}>
+                    Sign In
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -71,14 +92,8 @@ class Login extends Component {
   }
 }
 
-const UserOption = styled.option`
-  ::before{
-    background-image: url('${props => props.user.avatarURL}');
-  }
-`
-
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return { users: state.users }
 }
 
-export default connect(mapStateToProps)(Login)
+export default withRouter(connect(mapStateToProps)(Login))
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,33 +18,32 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <ul className="navbar-nav ml-5">
           <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="a#">
+            <NavLink className="nav-link" activeClassName="active" to="/">
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="E#">
+            <NavLink className="nav-link" activeClassName="active" to="/new">
               New Question
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="d#">
+            <NavLink className="nav-link" activeClassName="active" to="/leaderboard">
               Leader Board
             </NavLink>
           </li>
         </ul>
         {authedUser && (
           <div className="m-auto opacity-7">
+            <p className="my-0 d-inline-block p-2">Hello, {authedUser.name}</p>
             <img
               src={authedUser.avatarURL}
               alt="User avatar"
               className="user-avatar"
             />
-            <p className="my-0 d-inline-block p-2">Welcome {authedUser.name}</p>
             <button
-              className="btn align-baseline logout-btn p-2"
-              onClick={this.handleLogout}
-            >
+              className="btn align-baseline logout-btn p-2 ml-2"
+              onClick={this.handleLogout}>
               Logout
             </button>
           </div>
