591ae6797091b2bd89ae316acc432415d1da9a45
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-10-28 14:09:03 +0200

    Created questions reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux"
 import { loadingBarReducer } from "react-redux-loading-bar"
 import users from "./users"
 import authedUser from "./authedUser"
+import questions from "./questions"
 
 export default combineReducers({
   users,
   authedUser,
-  loadingBarReducer
+  questions,
+  loadingBar: loadingBarReducer,
 })
