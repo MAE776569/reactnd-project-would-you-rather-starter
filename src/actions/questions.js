47cb72a1832c2727d954c19c26620a848704c9b7
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-10-30 12:41:59 +0200

    Created save question actions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,7 +1,8 @@
 import { showLoading, hideLoading } from "react-redux-loading-bar"
-import { _getQuestions } from "../utils/_DATA"
+import { _getQuestions, _saveQuestionAnswer } from "../utils/_DATA"
 
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS"
+export const SAVE_QUESTION = "SAVE_QUESTION"
 
 function getQuestions(questions){
   return {
@@ -18,5 +19,26 @@ export function handleGetQuestions(){
         dispatch(getQuestions(questions))
         dispatch(hideLoading())
       })
+  }
+}
+
+function saveQuestion(question){
+  return {
+    type: SAVE_QUESTION,
+    question
+  }
+}
+
+export function handleSaveQuestion(authedUser, question, answer){
+  return (dispatch) => {
+    dispatch(showLoading())
+    return _saveQuestionAnswer({
+      authedUser,
+      qid: question.id,
+      answer
+    }).then((question) => {
+      dispatch(saveQuestion(question))
+      dispatch(hideLoading())
+    })
   }
 }
