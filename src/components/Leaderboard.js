d3afe6d58655e8bbcbbfdea9026fa9f359bd85ba
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-11-02 11:55:22 +0200

    Connected leaderboard to store

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
+import { Redirect } from "react-router-dom"
 
 class Leaderboard extends Component{
   render(){
+    const { authedUser, users } = this.props
+    if(!authedUser)
+      return <Redirect to="/login" />
+
     return (
       <div className="container mt-5">
         <div className="row justify-content-center">
           <div className="col-10 col-md-8 col-lg-6 p-0">
-            
+            <ul className="list-unstyled">
+              {users.map(user => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
@@ -15,4 +24,12 @@ class Leaderboard extends Component{
   }
 }
 
-export default connect()(Leaderboard)
+function mapStateToProps(state){
+  const users = Object.keys(state.users).map(user => state.users[user])
+  return {
+    users,
+    authedUser: state.authedUser
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard)
