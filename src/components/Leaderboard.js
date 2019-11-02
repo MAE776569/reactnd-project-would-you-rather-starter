5e57d0cf90c40725e7ab1ad57f7829cb690814b8
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-11-02 12:31:17 +0200

    Use leaderboard item

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { Redirect } from "react-router-dom"
+import LeaderboardItem from "./LeaderboardItem"
 
-class Leaderboard extends Component{
-  render(){
+class Leaderboard extends Component {
+  render() {
     const { authedUser, users } = this.props
-    if(!authedUser)
-      return <Redirect to="/login" />
+    if (!authedUser) return <Redirect to="/login" />
 
     return (
       <div className="container mt-5">
         <div className="row justify-content-center">
-          <div className="col-10 col-md-8 col-lg-6 p-0">
+          <div className="col-11 col-md-10 col-lg-7 p-0">
             <ul className="list-unstyled">
               {users.map(user => (
-                <li key={user.id}>{user.name}</li>
+                <LeaderboardItem key={user.id} user={user} />
               ))}
             </ul>
           </div>
@@ -24,8 +24,15 @@ class Leaderboard extends Component{
   }
 }
 
-function mapStateToProps(state){
-  const users = Object.keys(state.users).map(user => state.users[user])
+function mapStateToProps(state) {
+  const users = Object.keys(state.users)
+    .map(user => state.users[user])
+    .sort((a, b) => {
+      let totalFirst = Object.keys(a.answers).length + a.questions.length
+      let totalSecond = Object.keys(b.answers).length + b.questions.length
+      return totalSecond - totalFirst
+    })
+
   return {
     users,
     authedUser: state.authedUser
