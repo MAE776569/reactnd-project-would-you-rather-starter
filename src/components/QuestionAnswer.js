a6fbc05a963f681e1b15ca19c75f21133ccd98ef
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-11-04 12:44:34 +0200

    Convert to functional component

--- src/components/QuestionAnswer.js
+++ src/components/QuestionAnswer.js
@@ -1,88 +1,84 @@
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
 
-class QuestionAnswer extends Component {
-  render() {
-    const {
-      user,
-      question,
-      answer,
-      optionOneVotes,
-      optionTwoVotes,
-      totalVotes
-    } = this.props
+function QuestionAnswer(props) {
+  const {
+    user,
+    question,
+    answer,
+    optionOneVotes,
+    optionTwoVotes,
+    totalVotes
+  } = props
 
-    const divideBy = totalVotes ? totalVotes : 1
-
-    return (
-      <>
-        <div className="card-header">
-          <h5>Asked by {user.name}</h5>
+  return (
+    <>
+      <div className="card-header">
+        <h5>Asked by {user.name}</h5>
+      </div>
+      <div className="row no-gutters">
+        <div className="col-4 d-flex flex-wrap align-items-center">
+          <img src={user.avatarURL}
+            className="d-block m-auto question-avatar"
+            alt={`${user.name}'s avatar`} />
         </div>
-        <div className="row no-gutters">
-          <div className="col-4 d-flex flex-wrap align-items-center">
-            <img src={user.avatarURL}
-              className="d-block m-auto question-avatar"
-              alt={`${user.name}'s avatar`} />
-          </div>
-          <div className="col-8">
-            <div className="card-body">
-              <h5 className="card-title">Results:</h5>
-              <div
-                className={answer ? "card mb-3 selected-option" : "card mb-3"}>
-                <div className="card-body">
-                  {answer && (
-                    <span className="vote-badge badge badge-warning">
-                      Your vote
-                    </span>
-                  )}
-                  <div className="card-title font-weight-600">
-                    Would you rather {question.optionOne.text}?
-                  </div>
-                  <div className="progress">
-                    <div className="progress-bar bg-info"
-                      style={{ width: `${(optionOneVotes / divideBy) * 100}%` }}>
-                      {optionOneVotes
-                        ? `${((optionOneVotes / divideBy) * 100).toFixed(2)}%`
-                        : "" }
-                    </div>
-                  </div>
-                  <p className="text-center font-weight-600 mt-3">
-                    {`${optionOneVotes} out of ${totalVotes} votes`}
-                  </p>
+        <div className="col-8">
+          <div className="card-body">
+            <h5 className="card-title">Results:</h5>
+            <div
+              className={answer ? "card mb-3 selected-option" : "card mb-3"}>
+              <div className="card-body">
+                {answer && (
+                  <span className="vote-badge badge badge-warning">
+                    Your vote
+                  </span>
+                )}
+                <div className="card-title font-weight-600">
+                  Would you rather {question.optionOne.text}?
                 </div>
+                <div className="progress">
+                  <div className="progress-bar bg-info"
+                    style={{ width: `${(optionOneVotes / totalVotes) * 100}%` }}>
+                    {optionOneVotes
+                      ? `${((optionOneVotes / totalVotes) * 100).toFixed(2)}%`
+                      : "" }
+                  </div>
+                </div>
+                <p className="text-center font-weight-600 mt-3">
+                  {`${optionOneVotes} out of ${totalVotes} votes`}
+                </p>
               </div>
-              <div
-                className={!answer ? "card mb-3 selected-option" : "card mb-3"}>
-                <div className="card-body">
-                  {!answer && (
-                    <span className="vote-badge badge badge-warning">
-                      Your vote
-                    </span>
-                  )}
-                  <div className="card-title font-weight-600">
-                    Would you rather {question.optionTwo.text}?
-                  </div>
-                  <div className="progress">
-                    <div
-                      className="progress-bar bg-info"
-                      style={{ width: `${(optionTwoVotes / divideBy) * 100}%` }}>
-                      {optionTwoVotes
-                        ? `${((optionTwoVotes / divideBy) * 100).toFixed(2)}%`
-                        : "" }
-                    </div>
-                  </div>
-                  <p className="text-center font-weight-600 mt-3">
-                    {`${optionTwoVotes} out of ${totalVotes} votes`}
-                  </p>
+            </div>
+            <div
+              className={!answer ? "card mb-3 selected-option" : "card mb-3"}>
+              <div className="card-body">
+                {!answer && (
+                  <span className="vote-badge badge badge-warning">
+                    Your vote
+                  </span>
+                )}
+                <div className="card-title font-weight-600">
+                  Would you rather {question.optionTwo.text}?
                 </div>
+                <div className="progress">
+                  <div
+                    className="progress-bar bg-info"
+                    style={{ width: `${(optionTwoVotes / totalVotes) * 100}%` }}>
+                    {optionTwoVotes
+                      ? `${((optionTwoVotes / totalVotes) * 100).toFixed(2)}%`
+                      : "" }
+                  </div>
+                </div>
+                <p className="text-center font-weight-600 mt-3">
+                  {`${optionTwoVotes} out of ${totalVotes} votes`}
+                </p>
               </div>
             </div>
           </div>
         </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
 }
 
 function mapStateToProps(state, props) {
